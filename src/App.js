bd94fdededd8438c7e92426bd166b59f90faf6da
Abishkar Joshi <joshiabii19@gmail.com>
2023-05-12 17:45:00 +0545

    Css and routing managed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+import styles from "./App.module.css";
 
-function App() {
+const HomePage = () => {
+  return <h1>This is a Home Page</h1>;
+};
+const AboutPage = () => {
+  return <h1>This is a About Page</h1>;
+};
+const ContactPage = () => {
+  return <h1>This is a Contact Page</h1>;
+};
+
+const App = () => {
+  const navLinkStyles = {
+    color: "blue",
+    cursor: "pointer"
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav className="navbar">
+        <div className={styles.nav_brand}>
+          <Link to="/">Brand</Link>
+        </div>
+        <ul className="nav-lists">
+          <li>
+            <Link
+              to="/"
+              className={`nav-link ${styles.nav_link}`}
+              style={{ cursor: "pointer", color: "red" }}
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/about"
+              className={`nav-link ${styles.nav_link}`}
+              style={navLinkStyles}
+            >
+              About
+            </Link>
+          </li>
+          <li>
+            <Link to="/contact" className={`nav-link ${styles.nav_link}`}>
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+      </Routes>
+    </>
   );
-}
+};
 
 export default App;
